5aadea04ea686c48064b5baa661386a9e2c8563f
OneGangk <595586336@qq.com>
2016-09-29 13:50:29 +0800

    Initial Commit

---
+++ SelectImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectImageDemo
+//
+//  Created by likangding on 16/9/29.
+//  Copyright © 2016年 likangding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
